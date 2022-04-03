ac29261eeac251ad5c9096ae77c8f0e1156e90b7
LGIH <99771126+LGIH@users.noreply.github.com>
2022-04-02 23:00:09 -0600

    Update Problema2.js

--- ProblemasColab/Problema2.js
+++ ProblemasColab/Problema2.js
@@ -1,30 +1,29 @@
 function Problema2(){
-  var num1 = parseInt(document.getElementById("num1").value);
-  var num2 = parseInt(document.getElementById("num2").value);
-  var num3 = parseInt(document.getElementById("num3").value);
-
-  if (num1 == num2 && num1 == num3){
-     document.getElementById("resultado").innerHTML = ("num1");
-     }
-     else{
-       
-        if (num1 > num2){
-           if (num1 > num3){
-              document.getElementById("resultado").innerHTML = ("num1");
-              }
-              else{
-                 document.getElementById("resultado").innerHTML = ("num3");
-                 }
-        }
-        else{
-           if(num1 < num2){
-              if (num2 > num3){
-                 document.getElementById("resultado").innerHTML = ("num2");
-                 }
-                 else{
-                    document.getElementById("resultado").innerHTML = ("num3");
-                    }
-              }
-           }
-     }   
-}
+   var num1 = parseInt(document.getElementById("num1").value);
+   var num2 = parseInt(document.getElementById("num2").value);
+   var num3 = parseInt(document.getElementById("num3").value);
+ 
+   if (num1 == num2 && num1 == num3){
+      document.getElementById("resultado").innerHTML = (num1);
+      }
+      else{
+         if (num1 > num2){
+            if (num1 > num3){
+               document.getElementById("resultado").innerHTML = (num1);
+               }
+               else{
+                  document.getElementById("resultado").innerHTML = (num3);
+                  }
+         }
+         else{
+            if(num1 < num2){
+               if (num2 > num3){
+                  document.getElementById("resultado").innerHTML = (num2);
+                  }
+                  else{
+                     document.getElementById("resultado").innerHTML = (num3);
+                     }
+               }
+            }
+      }   
+ }
